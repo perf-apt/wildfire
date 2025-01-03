a0aaed23d5d53c5c1ec7c4a9dc59fc82098b1fd5
ashahid <ashahid@cloudera.com>
2025-01-02 17:05:36 -0800

    SPARK-49881. compilation error fix after merge

--- sql/connect/server/src/main/scala/org/apache/spark/sql/connect/planner/StreamingForeachBatchHelper.scala
+++ sql/connect/server/src/main/scala/org/apache/spark/sql/connect/planner/StreamingForeachBatchHelper.scala
@@ -108,7 +108,8 @@ object StreamingForeachBatchHelper extends Logging {
             args.df.asInstanceOf[Dataset[Any]]
           } else {
             // Recover the Dataset from the DataFrame using the encoder.
-            Dataset.apply(args.df.sparkSession, args.df.logicalPlan)(encoder)
+            Dataset.apply(args.df.sparkSession, args.df.logicalPlan)(encoder,
+              args.df.queryExecution.getRelations)
           }
           fn(ds, args.batchId)
         } catch {
