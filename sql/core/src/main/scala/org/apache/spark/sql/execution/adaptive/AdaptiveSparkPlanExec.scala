34b2d7f653e40e2eea0c875a7d8870eb6369b391
ashahid <ashahid@cloudera.com>
2025-04-10 15:30:26 -0700

    initial changes

--- sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/AdaptiveSparkPlanExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/AdaptiveSparkPlanExec.scala
@@ -859,7 +859,7 @@ case class AdaptiveSparkPlanExec(
       // First have a quick check in the `stageCache` without having to traverse down the node.
       context.stageCache.get(e.canonicalized) match {
         case Some(existingStage) if conf.exchangeReuseEnabled =>
-          val stage = reuseQueryStage(existingStage, e)
+          val stage = reuseQueryStage(existingStage, e, hasStreamSidePushdownDependent)
           val isMaterialized = stage.isMaterialized
           CreateStageResult(
             newPlan = stage,
@@ -887,7 +887,7 @@ case class AdaptiveSparkPlanExec(
               val queryStage = context.stageCache.getOrElseUpdate(
                 newStage.plan.canonicalized, newStage)
               if (queryStage.ne(newStage)) {
-                newStage = reuseQueryStage(queryStage, e)
+                newStage = reuseQueryStage(queryStage, e, hasStreamSidePushdownDependent)
               }
             }
             val isMaterialized = newStage.isMaterialized
@@ -1097,8 +1097,10 @@ case class AdaptiveSparkPlanExec(
 
   private def reuseQueryStage(
       existing: ExchangeQueryStageExec,
-      exchange: Exchange): ExchangeQueryStageExec = {
-    val queryStage = existing.newReuseInstance(currentStageId, exchange.output)
+      exchange: Exchange,
+      hasStreamSidePushdownDependent: Boolean): ExchangeQueryStageExec = {
+    val queryStage = existing.newReuseInstance(currentStageId, exchange.output,
+      hasStreamSidePushdownDependent)
     currentStageId += 1
     setLogicalLinkForNewQueryStage(queryStage, exchange)
     queryStage
